10f935453e68ad426db6bfb486fb3abc36cb1bf2
Crowdbotics <team@crowdbotics.com>
2021-08-05 09:52:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "black_limit_29501Identifier",
+  url: "https://black_limit_29501Identifier.botics.co",
+  api: "https://black_limit_29501Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
